015847945a09ff41db27fbd8602ae14b31e27293
littlemine <wxlwxl1993@zju.edu.cn>
2022-07-03 20:07:57 +0800

    build fix

--- openvdb/openvdb/points/AttributeArray.h
+++ openvdb/openvdb/points/AttributeArray.h
@@ -1813,7 +1813,7 @@ TypedAttributeArray<ValueType_, Codec_>::writeMetadata(std::ostream& os, bool ou
     uint8_t serializationFlags(0);
     Index size(mSize);
     Index stride(mStrideOrTotalSize);
-    bool strideOfOne(this->stride() == 1);
+    bool strideOfOne(TypedAttributeArray<ValueType_, Codec_>::stride() == 1);
 
     bool bloscCompression = io::getDataCompression(os) & io::COMPRESS_BLOSC;
 
